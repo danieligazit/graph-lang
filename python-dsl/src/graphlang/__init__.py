bf2eba8e5a725f786873a8f9cd84af686e1b7ff7
danieligazit <danieligazit@gmail.com>
2022-02-17 01:33:50 +0200

    Working with backend

--- python-dsl/src/graphlang/__init__.py
+++ python-dsl/src/graphlang/__init__.py
@@ -1,2 +1,4 @@
+from graphlang.consts import Direction
+from graphlang.query_builder import gt, var, get, traverse, QueryBuilder, Var
+from graphlang.ast_expressions import *
 
-from graphlang.query_builder import gt, var, get, traverse, QueryBuilder
